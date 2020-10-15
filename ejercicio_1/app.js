8a9fa79cf836cccb08b8e8f8515d450986b1292d
SergioRocha92 <69970438+SergioRocha92@users.noreply.github.com>
2020-10-15 00:58:25 -0300

    Add files via upload

---
+++ ejercicio_1/app.js
@@ -0,0 +1,10 @@
+const obtenerChiste = require("./library");
+
+function muestraChiste(chiste){
+    console.log(chiste[0].setup);
+    console.log(chiste[0].punchline);
+}
+
+
+
+obtenerChiste(muestraChiste);
